7ea337d04454c898422e11182314972048fc8b4d
Your Name <garveyshah1595@gmail.com>
2025-03-06 02:12:56 +0300

    Update dependancies

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,14 +5,13 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true,
-    port: 5173,
-    hmr: {
-      host: 'localhost',
-      protocol: 'ws'
-    },
-    watch: {
-      usePolling: true
+    port: 3000,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+        secure: false
+      }
     }
   }
 })
