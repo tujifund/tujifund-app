747f01bd914d4874f657bcc256b2a8d65d31e466
nymaina <annmaina614@gmail.com>
2025-03-06 14:54:07 +0300

    Add function handledashboard that Reads the session.If no session exists → Redirect to login.Otherwise, show user info to login

--- backend/auth/auth.go
+++ backend/auth/auth.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 
 	"github.com/gorilla/sessions"
@@ -24,19 +25,20 @@ func HandleGoogleLogin(w http.ResponseWriter, r *http.Request) {
 	Url := googleOauthConfig.AuthCodeURL("random-state-token")
 	http.Redirect(w, r, Url, http.StatusTemporaryRedirect)
 }
-func HandleGoogleCallback(w http.ResponseWriter, r *http.Request){
+
+func HandleGoogleCallback(w http.ResponseWriter, r *http.Request) {
 	code := r.URL.Query().Get("code")
-	if code == ""{
-		http.Error(w, "athuration code not found",http.StatusBadRequest)
+	if code == "" {
+		http.Error(w, "athuration code not found", http.StatusBadRequest)
 		return
 	}
-	token,err := googleOauthConfig.Exchange(r.Context(),code)
+	token, err := googleOauthConfig.Exchange(r.Context(), code)
 	if err != nil {
-		http.Error(w,"failed to exchange token",http.StatusBadRequest)
+		http.Error(w, "failed to exchange token", http.StatusBadRequest)
 	}
-	//fetch user info from google
-	client := googleOauthConfig.Client(r.Context(),token)
-	resp,err := client.Get(("https://www.googleapis.com/oauth2/v2/userinfo"))
+	// fetch user info from google
+	client := googleOauthConfig.Client(r.Context(), token)
+	resp, err := client.Get(("https://www.googleapis.com/oauth2/v2/userinfo"))
 	if err != nil {
 		http.Error(w, "Failed to get user info", http.StatusInternalServerError)
 		return
@@ -44,14 +46,23 @@ func HandleGoogleCallback(w http.ResponseWriter, r *http.Request){
 	defer resp.Body.Close()
 	var user struct {
 		Email string `json:"email"`
-		Name string `json:"name"`
+		Name  string `json:"name"`
 	}
 	json.NewDecoder(resp.Body).Decode(&user)
-	//create session
-	session,_ := store.Get(r, "sessionname")
+	// create session
+	session, _ := store.Get(r, "sessionname")
 	session.Values["Email"] = user.Email
 	session.Values["Name"] = user.Name
-	session.Save(r,w)
-	//redirect to dashboard
-	http.Redirect(w,r, "/dashboard",http.StatusSeeOther)
+	session.Save(r, w)
+	// redirect to dashboard
+	http.Redirect(w, r, "/dashboard", http.StatusSeeOther)
+}
+
+func HandleDashboard(w http.ResponseWriter, r *http.Request) {
+	session, _ := store.Get(r, "sesionname")
+	if session.Values["email"] == nil {
+		http.Redirect(w, r, "/login", http.StatusFound)
+		return
+	}
+	fmt.Printf("Welcome, %s! Your email is %s", session.Values["name"], session.Values["email"])
 }
