159e2c13e42d3ea841151e04994e711769b5dae4
nymaina <annmaina614@gmail.com>
2025-03-06 14:33:28 +0300

    Add function googlecallback that exchage code with token so that one can access user info from google,decode it and create a session for it them redirect use to the login dashboard

--- backend/auth/auth.go
+++ backend/auth/auth.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"net/http"
 
 	"github.com/gorilla/sessions"
@@ -22,4 +23,35 @@ var store = sessions.NewCookieStore([]byte("super-secret-key"))
 func HandleGoogleLogin(w http.ResponseWriter, r *http.Request) {
 	Url := googleOauthConfig.AuthCodeURL("random-state-token")
 	http.Redirect(w, r, Url, http.StatusTemporaryRedirect)
+}
+func HandleGoogleCallback(w http.ResponseWriter, r *http.Request){
+	code := r.URL.Query().Get("code")
+	if code == ""{
+		http.Error(w, "athuration code not found",http.StatusBadRequest)
+		return
+	}
+	token,err := googleOauthConfig.Exchange(r.Context(),code)
+	if err != nil {
+		http.Error(w,"failed to exchange token",http.StatusBadRequest)
+	}
+	//fetch user info from google
+	client := googleOauthConfig.Client(r.Context(),token)
+	resp,err := client.Get(("https://www.googleapis.com/oauth2/v2/userinfo"))
+	if err != nil {
+		http.Error(w, "Failed to get user info", http.StatusInternalServerError)
+		return
+	}
+	defer resp.Body.Close()
+	var user struct {
+		Email string `json:"email"`
+		Name string `json:"name"`
+	}
+	json.NewDecoder(resp.Body).Decode(&user)
+	//create session
+	session,_ := store.Get(r, "sessionname")
+	session.Values["Email"] = user.Email
+	session.Values["Name"] = user.Name
+	session.Save(r,w)
+	//redirect to dashboard
+	http.Redirect(w,r, "/dashboard",http.StatusSeeOther)
 }
