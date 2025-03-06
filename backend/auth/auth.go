4150299a32dc84a98b09fee51cfdbf4d24f4bc56
nymaina <annmaina614@gmail.com>
2025-03-06 15:02:55 +0300

    allow client secret to be public

--- backend/auth/auth.go
+++ backend/auth/auth.go
@@ -11,8 +11,8 @@ import (
 )
 
 var googleOauthConfig = &oauth2.Config{
-	ClientID:     " ",
-	ClientSecret: " ",
+	ClientID:     "235545557579-1il82tci3v7nu4hh8sjhv6tqsk043kfg.apps.googleusercontent.com",
+	ClientSecret: " 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",
 	RedirectURL:  "http://localhost:8080/auth/callback",
 	Scopes:       []string{"email", "profile"},
 	Endpoint:     google.Endpoint,
